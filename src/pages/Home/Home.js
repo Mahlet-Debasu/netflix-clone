f18bd26c6145713475802a00740c5cce1e24d6e4
Mahlet-Debasu <mahletdebasu0@gmail.com>
2025-03-08 22:11:28 +0000

    rows

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import Banner from '../../components/Banner/Banner'
+import RowList from '../../components/Rows/RowList/RowList'
+import Row from '../../components/Rows/Row/Row'
 const Home = () => {
   return (
     <>
       <Header />
-
       <Banner />
+      <Row />
+      <RowList />
+
       <Footer />
     </>
   );
